63a7eb67d06180b34dbe17e9cddcb3c645284e3c
ramiinberg <rami.inberg@gmail.com>
2022-08-01 13:51:43 +0300

    saving textarea text to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [text, setText] = useState('')
+
+  const handleTextChange = event => {
+    setText(event.target.value)
+  }
+
   return (
     <div className='App'>
-      <h1>Title</h1>
-      <textarea />
-      <h4>Time remaining</h4>
-      <button type='button'>Start the game</button>
-      <h1>Word count</h1>
+      <h1>How fast do you type?</h1>
+      <textarea value={text} onChange={handleTextChange} />
+      <h4>Time reminaing: ???</h4>
+      <button type='button'>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   )
 }
