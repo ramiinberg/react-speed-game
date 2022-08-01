c48ecf2a3e728af17d709205cfa362e2590a8122
ramiinberg <rami.inberg@gmail.com>
2022-08-01 15:41:42 +0300

    starting and ending game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,32 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const STARTING_TIME = 5
+
   const [text, setText] = useState('')
-  const [timer, setTimer] = useState(5)
+  const [timer, setTimer] = useState(STARTING_TIME)
   const [runCount, setRunCount] = useState(false)
+  const [words, setWords] = useState(0)
 
   const handleTextChange = event => {
     setText(event.target.value)
+  }
+
+  const startGame = () => {
+    setTimer(STARTING_TIME)
+    setRunCount(true)
+    setText('')
   }
 
   const calculateWords = newText => {
     const splitText = newText.trim().split(' ')
     const filteredWords = splitText.filter(word => word !== '')
     return filteredWords.length
+  }
+
+  const endGame = () => {
+    setRunCount(false)
+    setWords(calculateWords(text))
   }
 
   useEffect(() => {
@@ -23,7 +37,7 @@ function App() {
           setTimer(prevState => prevState - 1)
         }, 1000)
       } else {
-        setRunCount(false)
+        endGame()
       }
     }
   }, [timer, runCount])
@@ -31,12 +45,12 @@ function App() {
   return (
     <div className='App'>
       <h1>How fast do you type?</h1>
-      <textarea value={text} onChange={handleTextChange} />
+      <textarea value={text} onChange={handleTextChange} disabled={!runCount} />
       <h4>Time remaining: {timer}</h4>
-      <button type='button' onClick={() => setRunCount(true)}>
+      <button type='button' onClick={startGame} disabled={runCount}>
         Start
       </button>
-      <h1>Word count: ???</h1>
+      <h1>Word count: {words}</h1>
     </div>
   )
 }
